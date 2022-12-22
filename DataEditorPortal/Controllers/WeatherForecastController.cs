fdfb1ee5b29580890edc62f2e38b3c54a9635e4a
Harris <weiyh@novasoftware.cn>
2022-12-22 07:24:41 +0000

    rename DataEditorPortal to DataEditorPortal.Web

--- DataEditorPortal/Controllers/WeatherForecastController.cs
+++ DataEditorPortal/Controllers/WeatherForecastController.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace DataEditorPortal.Controllers
+namespace DataEditorPortal.Web.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -23,17 +19,17 @@ namespace DataEditorPortal.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
     }
 }
