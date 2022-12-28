116bb9d091000a6b7d9d668b0b003efe0d484bba
HarrisWei <weiyh@novasoftware.cn>
2022-12-28 18:28:46 +0800

    change theme

--- DataEditorPortal/ClientApp/src/app/app.component.ts
+++ DataEditorPortal/ClientApp/src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'data-editor-portal';
   bgColor = 'bg-primary';
 
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private messageService: MessageService,
+    private primengConfig: PrimeNGConfig
+  ) {}
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
 
   handleClick() {
     this.bgColor = 'bg-blue-500';
