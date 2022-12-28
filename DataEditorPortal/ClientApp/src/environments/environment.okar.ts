c3bb77071605489310c2d4797c483d9b15d18808
HarrisWei <weiyh@novasoftware.cn>
2022-12-28 11:07:20 +0800

    Remove old front end code

--- DataEditorPortal/ClientApp/src/environments/environment.okar.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  webHeaderMessage: 'AROK-ONLY TEST ENVIRONMENT',
-  webHeaderMessage2: 'This site is not a production environment. It is used for testing AR and OK areas only.'
-
-};
