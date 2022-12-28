cf2b1a529f9697a373007e1768302fe7ffa639ff
ranlin <mumu@irlin.cn>
2022-12-28 17:06:43 +0800

    fix init primeng primeflex

--- DataEditorPortal/ClientApp/src/app/app.component.ts
+++ DataEditorPortal/ClientApp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-editor-portal';
+  bgColor = 'bg-primary';
+
+  constructor(private messageService: MessageService) {}
+
+  handleClick() {
+    this.bgColor = 'bg-blue-500';
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Service Message',
+      detail: 'Via MessageService'
+    });
+    setTimeout(() => {
+      this.bgColor = 'bg-pink-500';
+    }, 1000);
+  }
 }
--- DataEditorPortal/ClientApp/src/app/app.module.ts
+++ DataEditorPortal/ClientApp/src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// primeNG components
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+
+import { HeaderComponent } from './header/header.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [AppComponent, HeaderComponent, NavMenuComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    AppRoutingModule,
+    ToastModule
+  ],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
