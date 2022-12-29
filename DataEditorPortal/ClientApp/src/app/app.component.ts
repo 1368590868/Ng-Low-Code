7b2dbc09179965aaf79973912db6c0807b56e509
ranlin <mumu@irlin.cn>
2022-12-29 18:43:37 +0800

    fix remove themes

--- DataEditorPortal/ClientApp/src/app/app.component.ts
+++ DataEditorPortal/ClientApp/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MessageService } from 'primeng/api';
 import { PrimeNGConfig } from 'primeng/api';
-import { ThemeService } from './theme.service';
 
 @Component({
   selector: 'app-root',
@@ -10,27 +8,9 @@ import { ThemeService } from './theme.service';
 })
 export class AppComponent implements OnInit {
   title = 'data-editor-portal';
-  bgColor = 'bg-primary';
-  inputValue = '#3F51B5';
 
-  constructor(
-    private messageService: MessageService,
-    private primengConfig: PrimeNGConfig,
-    private themeService: ThemeService
-  ) {}
+  constructor(private primengConfig: PrimeNGConfig) {}
   ngOnInit(): void {
     this.primengConfig.ripple = true;
-
-    // init the theme style
-    this.themeService.changePrimaryColor(this.themeService.primaryColor);
-  }
-
-  onSearch() {
-    this.handleClick();
-  }
-
-  handleClick() {
-    this.themeService.changePrimaryColor(this.inputValue);
-    console.log(1);
   }
 }
