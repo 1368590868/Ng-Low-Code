8db5855f63a7c4dafae8443ae138604f8b33ca90
HarrisWei <weiyh@novasoftware.cn>
2022-12-30 18:20:29 +0800

    update header and menu style

--- DataEditorPortal/ClientApp/src/app/header/header.component.ts
+++ DataEditorPortal/ClientApp/src/app/header/header.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  items!: MenuItem[];
+
+  constructor() {
+    this.items = [
+      {
+        label: 'Home',
+        routerLink: ['/']
+      },
+      {
+        label: 'About',
+        routerLink: ['/about']
+      },
+      {
+        label: 'Contact',
+        routerLink: ['/contact']
+      }
+    ];
+  }
+}
