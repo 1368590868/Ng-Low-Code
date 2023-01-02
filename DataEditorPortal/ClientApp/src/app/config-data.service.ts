1bd2d7c68effab7a10b27359558e77342eb48ab1
ranlin <mumu@irlin.cn>
2023-01-02 23:37:38 +0800

    feat add notifyService

--- DataEditorPortal/ClientApp/src/app/config-data.service.ts
+++ DataEditorPortal/ClientApp/src/app/config-data.service.ts
@@ -1,12 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigDataService {
   public _apiUrl: string;
-  constructor(private http: HttpClient, @Inject('API_URL') apiUrl: string) {
+  public durationMs = 5000;
+  constructor(
+    private http: HttpClient,
+    @Inject('API_URL') apiUrl: string,
+    private messageService: MessageService
+  ) {
     this._apiUrl = apiUrl;
   }
 
@@ -32,5 +39,56 @@ export class ConfigDataService {
     return this.http.post(`${this._apiUrl}site/menus`, null, {
       withCredentials: true
     });
+  }
+
+  getTableColumns(): any {
+    return this.http.get(
+      `${this._apiUrl}UniversalGrid/UserManagement/config/columns`,
+      {
+        withCredentials: true
+      }
+    );
+  }
+
+  // notifyService
+  notifySuccess(summary: string, detail: string) {
+    this.messageService.add({
+      severity: 'success',
+      summary: summary,
+      detail: detail,
+      life: this.durationMs
+    });
+  }
+  notifyInfo(summary: string, detail: string) {
+    this.messageService.add({
+      severity: 'info',
+      summary: summary,
+      detail: detail,
+      life: this.durationMs
+    });
+  }
+  notifyWarning(summary: string, detail: string) {
+    this.messageService.add({
+      severity: 'warn',
+      summary: summary,
+      detail: detail,
+      life: this.durationMs
+    });
+  }
+  notifyError(summary: string, detail: string) {
+    this.messageService.add({
+      severity: 'error',
+      summary: summary,
+      detail: detail,
+      life: this.durationMs
+    });
+  }
+
+  notifyErrorInPipe<T>(
+    err: any,
+    returnData?: { data: []; total: 0 } | any
+  ): Observable<T> {
+    this.notifyError(err.name || err.statusText, err.message || err.error);
+    return of(returnData);
   }
 }
