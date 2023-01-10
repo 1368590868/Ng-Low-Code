09c36f6ad78718b914ca1bae40baf1b44cfe461a
ranlin <mumu@irlin.cn>
2023-01-10 13:52:34 +0800

    fix table info action

--- DataEditorPortal/ClientApp/src/app/features/universal-grid-action/components/view-record-action/view-record-action.component.ts
+++ DataEditorPortal/ClientApp/src/app/features/universal-grid-action/components/view-record-action/view-record-action.component.ts
@@ -4,6 +4,10 @@ import {
   OnGridActionDialogShow
 } from '../../directives/grid-action.directive';
 
+export interface ViewColumn {
+  name?: string;
+  value?: string;
+}
 @Component({
   selector: 'app-view-record-action',
   templateUrl: './view-record-action.component.html',
@@ -13,8 +17,20 @@ export class ViewRecordActionComponent
   extends GridActionDirective
   implements OnGridActionDialogShow
 {
+  viewData: ViewColumn[];
+
+  constructor() {
+    super();
+    this.viewData = [];
+  }
   onDialogShow(): void {
     console.log('On dialog show');
     console.log(this.selectedRecords[0]);
+    if (this.selectedRecords[0] !== undefined) {
+      const key = Object.keys(this.selectedRecords[0]).map(key => {
+        return { name: key, value: this.selectedRecords[0][key] };
+      });
+      this.viewData = key;
+    }
   }
 }
