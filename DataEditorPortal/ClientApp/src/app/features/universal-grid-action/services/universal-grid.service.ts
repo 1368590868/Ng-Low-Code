697e97f45d73446d55210f8403ee86c6a1be6bca
HarrisWei <weiyh@novasoftware.cn>
2023-01-10 10:43:27 +0800

    universal grid service

---
+++ DataEditorPortal/ClientApp/src/app/features/universal-grid-action/services/universal-grid.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UniversalGridService {
+  public currentPortalItem = 'usermanagement';
+
+  public _apiUrl: string;
+  constructor(private http: HttpClient, @Inject('API_URL') apiUrl: string) {
+    this._apiUrl = apiUrl;
+  }
+
+  getTableColumns(): any {
+    return this.http.get(
+      `${this._apiUrl}UniversalGrid/${this.currentPortalItem}/config/columns`
+    );
+  }
+
+  getTableData(tableParams: any) {
+    return this.http.post<{ data: any[]; total: number }>(
+      `${this._apiUrl}UniversalGrid/${this.currentPortalItem}/data`,
+      tableParams
+    );
+  }
+
+  getSearchConfig(): any {
+    return this.http.get(
+      `${this._apiUrl}UniversalGrid/${this.currentPortalItem}/config/search`
+    );
+    // return this.http.get<any>('assets/customers-large.json');
+  }
+
+  getDetailConfig(): any {
+    return this.http.get<any>('assets/customers-large.json');
+  }
+}
