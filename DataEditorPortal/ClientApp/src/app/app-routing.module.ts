5e4b3f1646786ad8e797b5c89c0cf2bea09705e6
ranlin <mumu@irlin.cn>
2023-01-29 15:24:50 +0800

    fix error router

--- DataEditorPortal/ClientApp/src/app/app-routing.module.ts
+++ DataEditorPortal/ClientApp/src/app/app-routing.module.ts
@@ -13,7 +13,6 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent, canActivate: [RouterGuard] },
   { path: 'contact', component: ContactComponent, canActivate: [RouterGuard] },
   { path: 'login', component: LoginComponent },
-  { path: '**', component: ErrorPageComponent },
 
   {
     path: 'portal-item/:name',
@@ -30,7 +29,8 @@ const routes: Routes = [
         m => m.PortalManagementModule
       ),
     canActivate: [RouterGuard]
-  }
+  },
+  { path: '**', component: ErrorPageComponent }
 ];
 
 @NgModule({
