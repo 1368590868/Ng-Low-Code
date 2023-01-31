5ef8a339cc23e0c0f4afda3acf3e0e1a35c0cb1f
ranlin <mumu@irlin.cn>
2023-01-31 17:36:14 +0800

    fix add service

--- DataEditorPortal/ClientApp/src/app/core/directive/permission.directive.ts
+++ DataEditorPortal/ClientApp/src/app/core/directive/permission.directive.ts
@@ -1,22 +1,21 @@
 import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Directive({
   selector: '[appNgPermission]'
 })
 export class PermissionDirective {
-  // permission list
-  private hasPermission: any = {
-    edit: false,
-    update: true,
-    delete: false
-  };
   constructor(
     private viewContainerRef: ViewContainerRef,
-    private templateRef: TemplateRef<any>
+    private templateRef: TemplateRef<any>,
+    private userService: UserService
   ) {}
   @Input()
   set appNgPermission(value: any) {
-    if (this.hasPermission[value]) {
+    if (
+      this.userService.USER.permissions &&
+      this.userService.USER.permissions[value]
+    ) {
       this.viewContainerRef.createEmbeddedView(this.templateRef);
     } else {
       this.viewContainerRef.clear();
