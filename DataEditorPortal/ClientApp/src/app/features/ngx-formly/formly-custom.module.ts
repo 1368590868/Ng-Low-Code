1e72f20186e19cdcc5735667da7242add70e5cb1
HarrisWei <weiyh@novasoftware.cn>
2023-02-10 11:58:03 +0800

    create formly custom type module

---
+++ DataEditorPortal/ClientApp/src/app/features/ngx-formly/formly-custom.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { FormlyPrimeNGModule } from '@ngx-formly/primeng';
+import { FormlyCheckBoxModule } from './checkbox';
+import { FormlyChipWrapperModule } from './chip';
+import { FormlyDatepickerModule } from './datepicker';
+import { FormlyDividerWrapperModule } from './divider';
+import { FormlyIconSelectModule } from './iconselect';
+import { FormlyInputMaskModule } from './inputMask';
+import { FormlyInputNumberModule } from './inputnumber';
+import { FormlyMultiSelectModule } from './multiselect';
+import { FormlySelectModule } from './select';
+
+@NgModule({
+  imports: [
+    FormlyPrimeNGModule,
+    FormlyDatepickerModule,
+    FormlyMultiSelectModule,
+    FormlyInputMaskModule,
+    FormlySelectModule,
+    FormlyDividerWrapperModule,
+    FormlyCheckBoxModule,
+    FormlyChipWrapperModule,
+    FormlyIconSelectModule,
+    FormlyInputNumberModule
+  ]
+})
+export class FormlyCustomTypeModule {}
