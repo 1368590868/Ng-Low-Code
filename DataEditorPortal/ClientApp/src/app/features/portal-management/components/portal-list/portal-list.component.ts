5c8006341382c1e725c499d55c2864953d7938df
ranlin <mumu@irlin.cn>
2023-02-13 17:11:41 +0800

    fix right click menu error

--- DataEditorPortal/ClientApp/src/app/features/portal-management/components/portal-list/portal-list.component.ts
+++ DataEditorPortal/ClientApp/src/app/features/portal-management/components/portal-list/portal-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MenuItem, TreeNode } from 'primeng/api';
 import { ContextMenu } from 'primeng/contextmenu';
@@ -44,6 +44,7 @@ export class PortalListComponent implements OnInit {
   @ViewChild('addDialog') addDialog!: AddPortalDialogComponent;
   @ViewChild('cm') contextMenu!: ContextMenu;
   @ViewChild('customActions') customActions!: CustomActionsComponent;
+  @ViewChildren(Menu) menu!: Menu[];
 
   constructor(
     private router: Router,
@@ -65,6 +66,9 @@ export class PortalListComponent implements OnInit {
   }
 
   onContextMenuSelect(event: any) {
+    this.menu.forEach(res => {
+      res.hide();
+    });
     this.contextMenuItems = this.getMenuList(event.node);
     setTimeout(() => {
       this.contextMenu.show(event.originalEvent);
