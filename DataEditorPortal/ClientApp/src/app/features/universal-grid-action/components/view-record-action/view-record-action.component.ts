5211a9245805258d12aea45264dd87b039cb9e89
ranlin <mumu@irlin.cn>
2023-02-28 17:12:25 +0800

    replace header text & value format

--- DataEditorPortal/ClientApp/src/app/features/universal-grid-action/components/view-record-action/view-record-action.component.ts
+++ DataEditorPortal/ClientApp/src/app/features/universal-grid-action/components/view-record-action/view-record-action.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { GridColumn } from 'src/app/features/portal-management/models/portal-item';
+import { DataFormatService } from 'src/app/features/universal-grid/services/data-format.service';
+import { evalExpression, evalStringExpression } from 'src/app/shared/utils';
 import { GridActionDirective } from '../../directives/grid-action.directive';
+import { UniversalGridService } from '../../services/universal-grid.service';
 
-export interface ViewColumn {
+export interface ViewColumn extends GridColumn {
   name?: string;
   value?: string;
 }
@@ -16,22 +20,43 @@ export class ViewRecordActionComponent
 {
   viewData: ViewColumn[];
   loading = true;
+  formatters?: any;
 
-  constructor() {
+  constructor(
+    private universalGridService: UniversalGridService,
+    private dataFormatService: DataFormatService
+  ) {
     super();
     this.viewData = [];
+    this.formatters = this.dataFormatService.getFormatters();
   }
 
   ngOnInit(): void {
-    if (this.selectedRecords[0] !== undefined) {
-      const key = Object.keys(this.selectedRecords[0])
-        .filter(key => key != 'RowNumber')
-        .map(key => {
-          return { name: key, value: this.selectedRecords[0][key] };
-        });
-      this.viewData = key;
-      this.loading = false;
-      this.loadedEvent.emit();
-    }
+    this.universalGridService.getTableColumns().subscribe(res => {
+      if (this.selectedRecords[0] !== undefined) {
+        const key = Object.keys(this.selectedRecords[0])
+          .filter(key => key != 'RowNumber')
+          .map(key => {
+            const field = res.find(x => x.field === key);
+            return {
+              name: field?.header,
+              value: this.selectedRecords[0][key],
+              type: field?.type,
+              format: field?.format,
+              template: field?.template,
+              filterType: field?.filterType
+            };
+          })
+          .filter(x => x.name !== undefined);
+        this.viewData = key;
+        this.loading = false;
+        this.loadedEvent.emit();
+      }
+    });
+  }
+
+  calcCustomTemplate(data: any, template: string) {
+    const expression = evalStringExpression(template, ['row', 'pipes']);
+    return evalExpression(expression, data, [data, this.formatters]);
   }
 }
