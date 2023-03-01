08be77b09f34d391856c008f9a4fb106cb20eea5
ranlin <mumu@irlin.cn>
2023-03-01 11:56:59 +0800

    fix ts data type

--- DataEditorPortal/ClientApp/src/app/shared/models/system-log.ts
+++ DataEditorPortal/ClientApp/src/app/shared/models/system-log.ts
@@ -1,8 +1,26 @@
+import { FilterMetadata, SortMeta } from 'primeng/api';
+
+export interface SearchParam {
+  [name: string]: string;
+}
+
+export interface GridFilterParam extends FilterMetadata {
+  field: string;
+}
+
 export interface PaginationEvent {
   page?: number;
   first: number;
   rows: number;
   pageCount?: number;
+}
+
+export interface GridParam {
+  filters: GridFilterParam[];
+  sorts: SortMeta[];
+  searches?: SearchParam;
+  startIndex: number;
+  indexCount: number;
 }
 
 export interface SortMetaEvent {
