139de88a7c7808788cd7252b386e816953252a61
HarrisWei <weiyh@novasoftware.cn>
2023-03-22 18:17:53 +0800

    linked table

---
+++ DataEditorPortal.Web/ClientApp/src/app/features/universal-grid/components/linked-table/linked-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-linked-table',
+  templateUrl: './linked-table.component.html',
+  styleUrls: ['./linked-table.component.scss']
+})
+export class LinkedTableComponent {
+
+}
