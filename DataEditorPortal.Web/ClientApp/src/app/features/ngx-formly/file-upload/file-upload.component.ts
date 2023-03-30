63c5ae8955f36b6500e186066fe8a2827f50d0dc
ranlin <mumu@irlin.cn>
2023-03-30 11:14:52 +0800

    fix limit file

--- DataEditorPortal.Web/ClientApp/src/app/features/ngx-formly/file-upload/file-upload.component.ts
+++ DataEditorPortal.Web/ClientApp/src/app/features/ngx-formly/file-upload/file-upload.component.ts
@@ -72,16 +72,20 @@ export class FileUploadComponent implements ControlValueAccessor {
     this.apiUrl = apiUrl;
   }
 
+  onFileUploadSelect(event: any, AttachmentsRef: any) {
+    if (
+      event.currentFiles.length + this.newAttachments.length >
+      this.fileLimit
+    ) {
+      this.notifyService.notifyError('Error', 'File Limit Exceeded');
+      AttachmentsRef.clear();
+    } else {
+      AttachmentsRef.upload();
+    }
+  }
+
   onUpload(event: any) {
     if (event?.originalEvent?.body?.result) {
-      if (
-        this.fileLimit &&
-        this.newAttachments.length + event.originalEvent.body.result.length >
-          this.fileLimit
-      ) {
-        this.notifyService.notifyError('Error', 'File Limit Exceeded');
-        return;
-      }
       for (const file of event.originalEvent.body.result) {
         this.newAttachments.push(file);
       }
