754df14d236c285be3d58ad1568adbc02b3dfc9c
HarrisWei <weiyh@novasoftware.cn>
2023-04-24 17:14:03 +0800

    fix link table permission

--- DataEditorPortal.Web/ClientApp/src/app/features/universal-grid/universal-grid-routing.module.ts
+++ DataEditorPortal.Web/ClientApp/src/app/features/universal-grid/universal-grid-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SplitAreaComponent } from './pages/split-area/split-area.component';
+import { PermissionRouterGuard } from 'src/app/shared';
 
 const routes: Routes = [
   {
     path: 'single/:name',
     component: SplitAreaComponent,
-    data: { type: 'single' }
+    data: { type: 'single' },
+    canActivate: [PermissionRouterGuard]
   },
   {
     path: 'linked/:name',
     component: SplitAreaComponent,
-    data: { type: 'linked' }
+    data: { type: 'linked' },
+    canActivate: [PermissionRouterGuard]
   }
 ];
 
