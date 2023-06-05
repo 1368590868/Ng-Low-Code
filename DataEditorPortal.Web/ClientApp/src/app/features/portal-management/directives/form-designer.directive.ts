4437507dfb297f99bffddd179631063a12bd9f6d
ranlin <ranlin@novasoftware.cn>
2023-06-05 17:54:58 +0800

    fix location remove validate & computed

--- DataEditorPortal.Web/ClientApp/src/app/features/portal-management/directives/form-designer.directive.ts
+++ DataEditorPortal.Web/ClientApp/src/app/features/portal-management/directives/form-designer.directive.ts
@@ -165,7 +165,7 @@ export class FormDesignerDirective {
         }
       ],
       expressions: {
-        hide: `formState.hideComputedValue || 'fileUpload' === field.parent.model.type`
+        hide: `formState.hideComputedValue === field.parent.model.type || 'fileUpload' === field.parent.model.type || 'locationEditor'=== field.parent.model.type `
       }
     },
     {
@@ -411,7 +411,7 @@ export class FormDesignerDirective {
         }
       ],
       expressions: {
-        hide: `formState.hideValidation || 'fileUpload' === field.parent.model.type`
+        hide: `formState.hideValidation === field.parent.model.type || 'fileUpload' === field.parent.model.type || 'locationEditor' === field.parent.model.type `
       }
     }
   ];
