5591b1d9af5c99ec1c7714f866d16a744e6add20
ranlin <1368590868@qq.com>
2023-06-21 16:14:01 +0800

    fix remove assets file & notification replace

--- DataEditorPortal.Web/ClientApp/src/app/features/universal-grid-action/components/user-manager-action/user-manager-action.component.ts
+++ DataEditorPortal.Web/ClientApp/src/app/features/universal-grid-action/components/user-manager-action/user-manager-action.component.ts
@@ -192,15 +192,9 @@ export class UserManagerActionComponent
           type: 'checkbox',
           defaultValue: true,
           props: {
-            label: 'Receive Email Notifycations',
+            label: 'Receive Email Notifications',
             binary: true,
-            required: true,
-            options: [
-              {
-                value: 'Notify',
-                label: 'Notify'
-              }
-            ]
+            required: true
           }
         }
       ]
