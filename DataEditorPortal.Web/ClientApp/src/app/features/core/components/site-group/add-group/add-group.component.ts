4555b72d2110433eae9e65c30f1bff7b05a220cb
ranlin <1368590868@qq.com>
2023-07-27 11:26:30 +0800

    fix skeleton

--- DataEditorPortal.Web/ClientApp/src/app/features/core/components/site-group/add-group/add-group.component.ts
+++ DataEditorPortal.Web/ClientApp/src/app/features/core/components/site-group/add-group/add-group.component.ts
@@ -63,10 +63,111 @@ export class AddGroupComponent {
     if (this.id) {
       this.getDetail();
     } else {
+      this.getFields();
       this.options?.resetModel?.();
       this.formControlAboutEditor.reset();
       this.formControlContactEditor.reset();
     }
+  }
+
+  getFields() {
+    this.fields = [
+      {
+        fieldGroup: [
+          {
+            className: 'w-6',
+            key: 'title',
+            type: 'input',
+            props: {
+              label: 'Title',
+              placeholder: 'Title',
+              required: true
+            },
+            modelOptions: {
+              updateOn: 'blur'
+            },
+            hooks: {
+              onInit: field => {
+                field.formControl?.valueChanges
+                  .pipe(takeUntil(this.destroy$))
+                  .subscribe(val => {
+                    if (!val) return;
+                    if (field && field.parent && field.parent.get) {
+                      const control = field.parent?.get('name').formControl;
+                      control?.setValue(val);
+                    }
+                  });
+              }
+            }
+          },
+          {
+            className: 'w-6',
+            key: 'name',
+            type: 'input',
+            props: {
+              label: 'Name',
+              placeholder: 'Name',
+              required: true
+            },
+            modelOptions: {
+              updateOn: 'blur'
+            },
+            asyncValidators: {
+              exist: {
+                expression: (c: AbstractControl) => {
+                  const currVal = c.value;
+                  if (this.timer) clearTimeout(this.timer);
+                  return new Promise((resolve, reject) => {
+                    if (!currVal) {
+                      resolve(true);
+                    } else {
+                      this.timer = setTimeout(() => {
+                        this.siteGroupService
+                          .nameExists(currVal, this.id)
+                          .subscribe(res => {
+                            res.code === 200
+                              ? resolve(!res.data)
+                              : reject(res.message);
+                          });
+                      }, 100);
+                    }
+                  });
+                },
+                message: () => {
+                  return 'The Name has already been exist.';
+                }
+              }
+            },
+            hooks: {
+              onInit: field => {
+                field.formControl?.valueChanges
+                  .pipe(takeUntil(this.destroy$))
+                  .subscribe(val => {
+                    if (val) {
+                      this.siteGroupService.getCodeName(val).subscribe(res => {
+                        field.formControl?.setValue(res.data, {
+                          emitEvent: false
+                        });
+                        this.model['name'] = res.data + '';
+                        field.formControl?.markAsDirty();
+                      });
+                    }
+                  });
+              }
+            }
+          }
+        ]
+      },
+
+      {
+        key: 'description',
+        type: 'textarea',
+        props: {
+          label: 'Description',
+          placeholder: 'Description'
+        }
+      }
+    ];
   }
 
   getDetail() {
@@ -80,106 +181,7 @@ export class AddGroupComponent {
         );
         this.isLoading = false;
       }
-
-      this.fields = [
-        {
-          fieldGroup: [
-            {
-              className: 'w-6',
-              key: 'title',
-              type: 'input',
-              props: {
-                label: 'Title',
-                placeholder: 'Title',
-                required: true
-              },
-              modelOptions: {
-                updateOn: 'blur'
-              },
-              hooks: {
-                onInit: field => {
-                  field.formControl?.valueChanges
-                    .pipe(takeUntil(this.destroy$))
-                    .subscribe(val => {
-                      if (!val) return;
-                      if (field && field.parent && field.parent.get) {
-                        const control = field.parent?.get('name').formControl;
-                        control?.setValue(val);
-                      }
-                    });
-                }
-              }
-            },
-            {
-              className: 'w-6',
-              key: 'name',
-              type: 'input',
-              props: {
-                label: 'Name',
-                placeholder: 'Name',
-                required: true
-              },
-              modelOptions: {
-                updateOn: 'blur'
-              },
-              asyncValidators: {
-                exist: {
-                  expression: (c: AbstractControl) => {
-                    const currVal = c.value;
-                    if (this.timer) clearTimeout(this.timer);
-                    return new Promise((resolve, reject) => {
-                      if (!currVal) {
-                        resolve(true);
-                      } else {
-                        this.timer = setTimeout(() => {
-                          this.siteGroupService
-                            .nameExists(currVal, this.id)
-                            .subscribe(res => {
-                              res.code === 200
-                                ? resolve(!res.data)
-                                : reject(res.message);
-                            });
-                        }, 100);
-                      }
-                    });
-                  },
-                  message: () => {
-                    return 'The Name has already been exist.';
-                  }
-                }
-              },
-              hooks: {
-                onInit: field => {
-                  field.formControl?.valueChanges
-                    .pipe(takeUntil(this.destroy$))
-                    .subscribe(val => {
-                      if (val) {
-                        this.siteGroupService
-                          .getCodeName(val)
-                          .subscribe(res => {
-                            field.formControl?.setValue(res.data, {
-                              emitEvent: false
-                            });
-                            this.model['name'] = res.data + '';
-                            field.formControl?.markAsDirty();
-                          });
-                      }
-                    });
-                }
-              }
-            }
-          ]
-        },
-
-        {
-          key: 'description',
-          type: 'textarea',
-          props: {
-            label: 'Description',
-            placeholder: 'Description'
-          }
-        }
-      ];
+      this.getFields();
       this.isLoading = false;
     });
   }
